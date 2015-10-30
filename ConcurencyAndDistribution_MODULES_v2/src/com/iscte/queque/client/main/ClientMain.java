5c90885d2b7cf2e4211221bd5e31ed41ec9ec348
atafs <atafs@iscte-iul.pt>
2015-10-30 21:49:16 +0000

    update

---
+++ ConcurencyAndDistribution_MODULES_v2/src/com/iscte/queque/client/main/ClientMain.java
@@ -0,0 +1,11 @@
+package com.iscte.queque.client.main;
+
+import com.iscte.queque.client.gui.ClientFrame;
+
+public class ClientMain {
+	public static void main(String[] args) {
+
+		//INSTANTIATE
+		new ClientFrame().setVisible(true);
+	}
+}
---
+++ ConcurencyAndDistribution_MODULES_v2/src/com/iscte/queque/client/main/ServerMain.java
@@ -0,0 +1,11 @@
+package com.iscte.queque.client.main;
+
+import com.iscte.queque.server.service.ServidorService;
+
+public class ServerMain {
+	public static void main(String[] args) {
+	
+		//INSTANTIATE
+		new ServidorService();
+	}
+}
