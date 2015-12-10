9ed523744d82f221df0d40e1322872458b50ade9
atafs <atafs@iscte-iul.pt>
2015-12-10 01:24:43 +0000

    divide the project: server and client

---
+++ ConcurencyAndDistribution_MODULES_v2_SERVER/src/com/iscte/queque/client/main/ServerMain.java
@@ -0,0 +1,11 @@
+package com.iscte.queque.client.main;
+
+import com.iscte.queque.client.service.ServerService;
+
+public class ServerMain {
+	public static void main(String[] args) {
+	
+		//INSTANTIATE
+		new ServerService();
+	}
+}
---
+++ ConcurencyAndDistribution_MODULES_v2_SERVER/src/com/iscte/queque/controlServerGUI/DataPackage.java
@@ -0,0 +1,14 @@
+package com.iscte.queque.controlServerGUI;
+
+import java.io.Serializable;
+
+@SuppressWarnings("serial")
+public class DataPackage implements Serializable{
+	
+	/*attributes*/
+	public float x = 0.0f;
+	public float y = 0.0f;
+	
+	public String username = "";
+
+}
