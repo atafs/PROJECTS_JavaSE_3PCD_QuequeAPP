088cb80f76365351991090d860b54181b3c05121
atafs <atafs@iscte-iul.pt>
2015-12-12 16:13:34 +0000

    change names

--- ConcurrencyAndDistribution_SERVER_vRebuild_1DEV/src/com/iscte/queque/main/ServerMain.java
+++ ConcurrencyAndDistribution_SERVER_vRebuild_1DEV/src/com/iscte/queque/main/ServerMain.java
@@ -1,11 +1,13 @@
 package com.iscte.queque.main;
 
+import com.iscte.queque.thread.ServerService;
+
 public class ServerMain {
 	
 	//MAIN
 	public static void main(String[] args) {
 		//INSTANTIATE
-
+		new ServerService();
 	}
 
 }
